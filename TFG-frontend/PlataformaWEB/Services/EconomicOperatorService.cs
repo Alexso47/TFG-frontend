0bf167a4998262b08cc0ffb9ec0a9afb58aec705
AlejandroSanchezO <alejandro.sanchezo@e-campus.uab.cat>
2020-06-08 00:49:22 +0200

    no message

--- TFG-frontend/PlataformaWEB/Services/EconomicOperatorService.cs
+++ TFG-frontend/PlataformaWEB/Services/EconomicOperatorService.cs
@@ -26,7 +26,7 @@ namespace PlataformaWEB.Services
         public EconomicOperatorService(HttpClient httpClient, IOptions<AppSettings> settings)
         {
             _httpClient = httpClient;
-            _remoteServiceBaseUrl ="http://localhost:5001/";
+            _remoteServiceBaseUrl = "https://apitfgalex.azurewebsites.net/";
         } 
 
         async public Task<int> Create(EconomicOperator economicOperator)
